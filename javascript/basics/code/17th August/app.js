e0641a583e76c1e21da0abad3d357bb21c006e71
InnoSufiyan <innosufiyan@gmail.com>
2024-08-17 16:53:38 +0500

    javascript

---
+++ javascript/basics/code/17th August/app.js
@@ -0,0 +1,62 @@
+// var bossWords = "furqan";
+
+// alert("bossWords");
+// alert(bossWords);
+// alert(bossWords);
+// // alert(bossWords)
+// // alert(bossWords)
+// // alert(bossWords)
+// // alert(bossWords)
+// alert(bossWords);
+
+// es5 //book
+
+//es5 + es6 and other advance topics from Slides
+
+// let const var  -->> es6
+
+// var schoolName; // -->> declaration
+
+// console.log(schoolName);
+
+// schoolName = "Happy Palace"; // -->> assignment // initialization
+
+// var schoolName = "Happy Palace"; // -->> declaration + assignment  initialize
+// console.log(schoolName);
+// schoolName = "Happy Home"; // re-assignment // re-initialize
+
+// console.log(schoolName);
+
+// schoolName = "Habib Girls School";
+
+// console.log(schoolName);
+
+// var beta = 'Abdur Rehman'; // declare + assignment // initialization
+
+// console.log(beta, ' peda huwa hai')
+
+// beta = 'Abdur Raheem'  // assignment
+
+// let beta = "Abdur Rehman"; -->> only can be declared once
+
+// const gender = "larka"; -->> declaration and assignment one time only
+
+const fatherName = "Waris Khan"; // at the time of declaration assignment is necessary
+
+// var -->> many declrations , many re-assignments allowed
+// let -->> one time declaration, many re-assignments allowed
+// const -->> one time declaration, one assignment allowed -->> no re-assignment
+
+// var parosiBachaSchool = "Bagh e Halar";
+
+// const hamareyBachaSchool = parosiBachaSchool; // qasam kha li school change nai karengey
+
+// console.log(parosiBachaSchool);
+// console.log(hamareyBachaSchool);
+
+// parosiBachaSchool = "Happy Palace";
+
+// console.log(parosiBachaSchool);
+// console.log(hamareyBachaSchool);
+
+
