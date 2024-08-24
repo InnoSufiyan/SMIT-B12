1149c06081ce6ca68a2920bc087579607dd948ac
InnoSufiyan <innosufiyan@gmail.com>
2024-08-24 17:45:03 +0500

    slides function 21

---
+++ javascript/basics/code/19th August/app.js
@@ -0,0 +1,48 @@
+//Array   -->> [value1, value2, value3, value4]
+// separate -->> juda karna
+//comma separated
+
+// var student1Name = "Uzair";
+// var student2Name = "Farasat";
+// var student3Name = "Mehdi";
+// var student4Name = "Taha";
+
+// var students = ["Uzair", "Farasat", "Mehdi", "Taha"];
+// console.log(students, "==>> students")
+
+//Array is just like a Cupboard || Shelf
+
+//Resturant
+
+// var resturant = [
+//   "biryani",
+//   "korma",
+//   "pulao",
+//   "nihari",
+//   "tikka",
+//   "spegghati",
+//   "malai boti",
+//   "paey",
+// ];
+
+// var customerDemand = +prompt("Haan bhae kya khaogey");
+// console.log(customerDemand, "==>> customerDemand")
+
+// document.write(resturant[customerDemand]);
+
+//Functions -->> Machine
+
+function betaPaniLeKarAao() {
+  console.log("Haramain beta, zara room se bahir niklo");
+  console.log("samney cupboard k barabar main, ek water cooler hoga");
+  console.log("us k pass jao");
+  console.log(
+    "is k barabar main ek glass hoga, us ko bari ehtiyat k sath uthao"
+  );
+  console.log("water cooler ki ek tooti hogi, us k neechey is glass ko rakhna");
+  console.log("tooti ko on karna, glass bharney ka wait karna");
+  console.log("glass pani se bhar jaey to tooti band kardena");
+  console.log("pani ka glass le kar wapis aajana");
+}
+
+betaPaniLeKarAao()
